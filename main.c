2e1034bc8955673bf1a09768e4888d564b667f9b
ross96D <abelaross96@gmail.com>
2024-03-02 17:04:04 -0500

    fix: add error handling for fopen call

--- main.c
+++ main.c
@@ -28,9 +28,34 @@ void create(const char *path, const char sep)
         // move to the next character
         p++;
     }
-    //! TODO.. maybe this need error handling.. i dont know
-    fclose(fopen(subpath, "a"));
-    free(subpath); // free the memory
+
+    FILE *ptr = fopen(subpath, "a");
+    if (ptr == NULL)
+    {
+        if (errno == ENOTDIR)
+        {
+            printf("error: trying to create a file inside a file\n");
+        }
+        else
+        {
+            perror("");
+        }
+        exit(EXIT_FAILURE);
+    }
+    fclose(ptr);
+    free(subpath);
+}
+
+int check_is_file(const char *path)
+{
+    char *p = path;
+    char last = *p;
+    while (*p != 0)
+    {
+        last = *p;
+        p++;
+    }
+    return last != 0 && last != '/';
 }
 
 int main(int argc, char const *argv[])
@@ -40,6 +65,13 @@ int main(int argc, char const *argv[])
         printf("this take exactly one argument\n");
         return 1;
     }
+
+    if (!check_is_file(argv[1]))
+    {
+        printf("argument is not a file\n");
+        return 1;
+    }
+
     create(argv[1], '/');
 
     return 0;
